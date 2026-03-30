0dcea4c297c68931f5f12d12fa765bf0543d2ee4
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-03-30 19:02:08 -0400

    Make numbers show up in the display when clicked

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,17 @@
 // initialize variables
 const numbers = document.querySelectorAll("button");
+const display = document.querySelector(".result");
 
 numbers.forEach((button) => {
     if(!button.classList.contains("operator") && button.id !== "blank") {
-        button.addEventListener("click", () => console.log(button.id));
+        button.addEventListener("click", () => displayNumber(button));
     }
 });
+
+function displayNumber(num) {
+    display.textContent = num.id;
+}
+
+function add(num1, num2) {
+
+}
