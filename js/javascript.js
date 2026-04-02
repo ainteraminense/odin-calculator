44bdcf4a287992c10db161ec6e5da77645e4eaa1
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-04-02 09:14:04 -0400

    Operate all operations by choosing 1st num, operator, 2nd num and cliking equal sign to display result

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,12 @@
 // initialize variables
 const numbers = document.querySelectorAll("button");
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".result");
-const additionBtn = document.querySelector("#addition");
+const equalSign = document.querySelector("#equal-sign");
 let isAdditionActive = false;
+let isSubtractionActive = false;
+let isMultiplicationActive = false;
+let isDivisionActive = false;
 let isNum1Selected = false;
 let num1;
 let num2;
@@ -52,12 +56,9 @@ function selectNum1(event) {
 function selectNum2(event) {
     if (isNum1Selected) {
         event.target.removeEventListener("click", selectNum1);
-        if (isAdditionActive) {
-            num2 = Number(event.target.id);
-            console.log("num2: " + num2);
-            add(num1, num2);
-        }
-        }
+        num2 = Number(event.target.id);
+        console.log("num2: " + num2);
+    }
 }
 
 function displayNumber(num) {
@@ -73,12 +74,72 @@ function add(num1, num2) {
     displayResult(result);
 }
 
-addSelect1EventListeners();
+function subtract(num1, num2) {
+    result = num1 - num2;
+    console.log("result is: " + result);
+    displayResult(result);
+}
 
-additionBtn.addEventListener("click", () => {
-    isAdditionActive = true;
-    console.log("Is Addition on: " + isAdditionActive);
-    removeSelect1EventListeners();
-    addSelect2EventListeners();
+function multiply(num1, num2) {
+    result = num1 * num2;
+    displayResult(result);
+}
+
+function divide(num1, num2) {
+    result = num1 / num2;
+    displayResult(result);
+}
+
+function setOperator() {
+    if (selectNum1) {
+        operators.forEach((button) => {
+            button.addEventListener("click", () => {
+            switch (button.id) {
+                case "addition":
+                    isAdditionActive = true;
+                    console.log("Is Addition on: " + isAdditionActive);
+                    removeSelect1EventListeners();
+                    addSelect2EventListeners();
+                    break;
+                case "subtraction":
+                    isSubtractionActive = true;
+                    console.log("Is Subtraction on: " + isSubtractionActive);
+                    removeSelect1EventListeners();
+                    addSelect2EventListeners();
+                    break;
+                case "multiplication":
+                    isMultiplicationActive = true;
+                    console.log("Is Multiplication on: " + isMultiplicationActive);
+                    removeSelect1EventListeners();
+                    addSelect2EventListeners();
+                    break;
+                case "division":
+                    isDivisionActive = true;
+                    console.log("Is Division on: " + isDivisionActive);
+                    removeSelect1EventListeners();
+                    addSelect2EventListeners();
+                    break;
+                default: 
+                console.log("Equal sign was pressed");
+            }
+        })
+    })
+}};
+
+function operate() {
+    if (isAdditionActive) {
+        add(num1, num2);
+    } else if (isSubtractionActive) {
+        subtract(num1, num2);
+    } else if (isMultiplicationActive) {
+        multiply(num1, num2);
+    } else if (isDivisionActive) {
+        divide(num1, num2);
+    } else {
+        console.log("Something went wrong");
     }
-);
+}
+
+addSelect1EventListeners();
+setOperator();
+equalSign.addEventListener("click", operate)
